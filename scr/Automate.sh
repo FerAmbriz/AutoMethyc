33e52c400d70f8e435a70bf215c8e5a7fb1622c7
FerAmbriz <fernandoambrizbarrera@gmail.com>
2022-06-15 10:16:39 -0500

    Mejora en la incorporacion de flags

--- scr/Automate.sh
+++ scr/Automate.sh
@@ -1,12 +1,25 @@
 #!/bin/bash
 
-input=$1
-output=$2
-ref=$3
-filtro=$4
-
-#bash Automate.sh ../example/fastq/ .. ../example/ref/ ../example/Filtro2.csv
-
+while [[ $# -gt 0 ]]; do
+opt="$1"
+shift;
+current_arg="$1"
+case "$opt" in
+"-h"|"--help"      ) echo '''
+AutoMet version 0.0.1-beta
+	bash Automate.sh [options]
+Options
+	-i --input		Folder with fastq
+	-o --output		Output location
+	-r --ref		Folder with references
+	-f --filtro		File with regions of interest
+''';  exit 1;;
+"-i"|"--input"      ) input="$1"; shift;;
+"-o"|"--output"      ) output="$1"; shift;;
+"-r"|"--ref"      ) ref="$1"; shift;;
+"-f"|"--filtro"      ) filtro="$1"; shift;;
+	esac
+done
 bash BuclePipeline.sh $input $output $ref
 
 # descomprime y renombra los archivos por ID
