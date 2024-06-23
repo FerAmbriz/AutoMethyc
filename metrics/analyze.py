3a77f9107c4311e195ec1975d7ce24da9e1aacff
FerAmbriz <107364887+FerAmbriz@users.noreply.github.com>
2024-06-23 13:44:10 -0600

    Update analyze.py

--- metrics/analyze.py
+++ metrics/analyze.py
@@ -24,6 +24,9 @@ df_0['a'] = lst
 df = df_0[[0, 'a']]
 df.columns = ['Date', 'Memory']
 
+df = df[df['Date'] != 'Average:']
+
+df['Date'] = pd.to_datetime(df['Date'], format='%H:%M:%S')
 print(df)
 
 
@@ -31,4 +34,21 @@ df_2 = pd.read_csv('/home/lab13/Documents/FernandoAmbriz/example/output/executio
 
 df_2 = df_2[0].str.split('at: ', expand=True)
 df_2.columns = ['Command', 'Date']
+
+df_2['Date'] = pd.to_datetime(df_2['Date'], format='%H:%M:%S')
+
 print(df_2.head())
+
+k = 0; lst = []
+while k < len(df_2) - 1:
+    start = df_2['Date'][k]; end = df_2['Date'][k+1]
+    for i,j in zip(df.Date, df.Memory):
+        if start <= i <= end:
+            c = df_2['Command'][k]
+            lst.append([start, end, i, c, j])
+    k+=1
+
+df_final = pd.DataFrame(lst)
+df_final.columns = ['Start', 'End', 'Second', 'Command', 'Memory']
+
+print(df_final)
